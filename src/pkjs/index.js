f287f71726cdd57078201812c836fffd71fbdad5
Manta <111002655+MantaFirefly@users.noreply.github.com>
2025-04-10 13:25:53 +0100

    Update index.js
    Supports shocker ID and vibrate commands

--- src/pkjs/index.js
+++ src/pkjs/index.js
@@ -18,15 +18,25 @@ Pebble.addEventListener('appmessage', function (e) {
     var dict = e.payload;
 
     if (dict["shock_str"] && dict["shock_dur"]) {
-        sendShock(dict["shock_str"], dict["shock_dur"]);
+        sendShock(dict["shock_str"], dict["shock_dur"], dict["ID"], dict["mode"]);
     }
 });
 
-function sendShock(shock_str, shock_dur) {
+function sendShock(shock_str, shock_dur, ID, mode) {
     var settings = JSON.parse(localStorage.getItem("clay-settings"));
     if (settings && settings.api_key && settings.shocker_id) {
         api_key = settings.api_key;
+
+		if (ID == 3){
+		shocker_id = settings.shocker_id3;
+		}
+		else if (ID == 2){
+		shocker_id = settings.shocker_id2;
+		}
+		else {
         shocker_id = settings.shocker_id;
+		}
+		
     }
     else {
         console.log("VARIABLES UNSET IN CONFIG")
@@ -40,18 +50,35 @@ function sendShock(shock_str, shock_dur) {
     xhr.setRequestHeader("OpenShockToken", api_key);
     xhr.setRequestHeader("accept", "application/json");
     xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.send(JSON.stringify({
-        shocks: [
-            {
-                id: shocker_id,
-                type: "Shock",
-                intensity: shock_str,
-                duration: shock_dur,
-                exclusive: true
-            }
-        ],
-        customName: "Pebble App"
+	if (mode == 1) {
+		xhr.send(JSON.stringify({
+			shocks: [
+				{
+					id: shocker_id,
+					type: "Shock",
+					intensity: shock_str,
+					duration: shock_dur,
+					exclusive: true
+				}
+			],
+			customName: "Pebble App"
     }));
+	}
+		else
+		{
+		xhr.send(JSON.stringify({
+			shocks: [
+				{
+					id: shocker_id,
+					type: "Vibrate",
+					intensity: shock_str,
+					duration: shock_dur,
+					exclusive: true
+				}
+			],
+			customName: "Pebble App"
+    }));
+	}
     var response = JSON.parse(xhr.responseText);
     if (response["message"] != "Successfully sent control messages") {
         console.log("API Error: ", response["message"])
